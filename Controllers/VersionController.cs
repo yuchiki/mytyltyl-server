7639877b5503fa28342fa021c4cc4ab740f34884
yuchiki <14884013+yuchiki@users.noreply.github.com>
2022-12-25 13:27:50 +0000

    まともなlog

--- Controllers/VersionController.cs
+++ Controllers/VersionController.cs
@@ -17,7 +17,7 @@ namespace mytyltyl_server.Controllers
         [HttpGet(Name = "GetVersion")]
         public VersionResponse Get()
         {
-            _logger.LogInformation("GetVersion");
+            _logger.LogInformation("[{DT}] GET /Version", DateTime.UtcNow.ToLongTimeString());
             return new VersionResponse { MajorVersion = 0, MinorVersion = 0, PatchVersion = 0, StartedAt = Process.GetCurrentProcess().StartTime };
         }
     }
